8b6ad752cd2863598319fcce2211e51e8cac68ff
GioTFG <168219065+GioTFG@users.noreply.github.com>
2025-11-06 18:22:50 +0100

    Commented the decision on using subclasses for platform types.

--- src/actors/platforms.py
+++ src/actors/platforms.py
@@ -21,6 +21,13 @@ class BackgroundActor(Actor):
     def sprite(self):
         return None
 
+
+"""
+Ho creato questo sistema di sottoclassi per poter distinguere bene i vari tipi di piattaforma che ci sono nel gioco-
+(ad esempio, distinguere per lo zombie il terreno (Ground) dalle tombe o altri ostacoli (Grave)).
+Sebbene potessi creare per una singola classe un attributo (ad esempio, self._type = "Grave"), ho preferito creare
+delle sottoclassi, seppur eventualmente vuote, perché più flessibili nella possibilità di estendere i loro comportamenti.
+"""
 class BackgroundPlatform(BackgroundActor):
     pass
 
