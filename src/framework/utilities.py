1dc5ff4f04a639577798e090326b9ec31fbe8f40
GioTFG <168219065+GioTFG@users.noreply.github.com>
2025-11-20 19:07:09 +0100

    Added documentation

--- src/framework/utilities.py
+++ src/framework/utilities.py
@@ -2,17 +2,17 @@ from .actor import Point
 
 def center(pos: Point, size: Point) -> Point:
     """
-    Calcola il pixel "centrale" data posizione (in alto a sinistra) e dimensione (per avere l'angolo in basso a destra.
-    :param pos: La posizione, cioè il pixel più in alto a destra dell'oggetto.
-    :param size: La dimensione, cioè la quantità di pixel di larghezza e di altezza dell'oggetto
-    :return: La posizione (assoluta) del pixel centrale.
+    Calculates the central pixel of a given rectangle, given its top-left pixel position and its size.
+    :param pos: The position of the top-left pixel.
+    :param size: The size, so a tuple that says width and height of the rectangle.
+    :return: The (absolute) position of the central pixel.
     """
     x, y, w, h = pos + size
     return x + w / 2, y + h / 2
 
 def remove_pos(pos1: Point, pos2: Point) -> Point:
     """
-    Sottrae componente per componente da pos1 pos2.\n
+    Makes a subtraction between two points, component-by-component.\n
     Es: (10, 9) - (4, 1) = (6, 8)
     """
     return pos1[0] - pos2[0], pos1[1] - pos2[1]
